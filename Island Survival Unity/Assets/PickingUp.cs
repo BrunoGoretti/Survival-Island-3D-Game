10292c15e7252878d194dd525ff309281b4e444a
BrunoGoretti <104521859+BrunoGoretti@users.noreply.github.com>
2023-05-27 01:18:26 +0400

    Add features

---
+++ Island Survival Unity/Assets/PickingUp.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickingUp : MonoBehaviour
+{
+    public GameObject Item;
+    public bool canPickUp;
+    public int sticks;
+    public GameObject pickUpText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sticks = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canPickUp == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Destroy(Item);
+                Item = null;
+                sticks += 1;
+                canPickUp = false;
+            }
+        }
+
+        if (canPickUp == true)
+        {
+            pickUpText.SetActive(true);
+        }
+        else
+        {
+            pickUpText.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "item")
+        {
+            Item = col.gameObject;
+            canPickUp = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "item")
+        {
+            Item = null;
+            canPickUp = false;
+        }
+    }
+}
---
+++ Island Survival Unity/Assets/PlayerStats.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int maxHealt;
+    public int maxHunger;
+    public int maxStamina;
+
+    public Image Healt;
+    public Image Hunger;
+    public Image Stamina;
+
+    public float  HealInCreasedPerSecond;
+    public float  HungerInCreasedPerSecond;
+    public float  StaminaInCreasedPerSecond;
+
+    public Text HealthText;
+    public Text HungerText;
+    public Text StaminaText;
+
+    public float UpdatedHealth;
+    public float UpdatedHunger;
+    public float UpdatedStamina;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealInCreasedPerSecond = 5f;
+        HungerInCreasedPerSecond = -1f;
+        StaminaInCreasedPerSecond = 1f;
+
+        maxHealt = 1000;
+        maxHunger = 1000;
+        maxStamina = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatedHealth += HealInCreasedPerSecond * Time.deltaTime;
+        Healt.fillAmount = UpdatedHealth / maxHealt;
+
+        UpdatedHunger += HungerInCreasedPerSecond * Time.deltaTime;
+        Hunger.fillAmount = UpdatedHunger / maxHunger;
+
+        UpdatedStamina += StaminaInCreasedPerSecond * Time.deltaTime;
+        Stamina.fillAmount = UpdatedStamina / maxStamina;
+
+        HealthText.text = (int)UpdatedHealth + " ";
+        HungerText.text = (int)UpdatedHunger + " ";
+        StaminaText.text = (int)UpdatedStamina + " ";
+
+        if(UpdatedHealth >= maxHealt)
+        {
+            UpdatedHealth = maxHealt;
+        }
+        
+        if(UpdatedHunger >= maxHunger)
+        {
+            UpdatedHunger = maxHunger;
+        }
+        
+        if(UpdatedStamina >= maxStamina)
+        {
+            UpdatedStamina = maxStamina;
+        }
+    }
+}
